69e6dedd9e4d5f7b160a3af162c271924ddecb09
TechnologyEnthusiast <TechnologyEnthusiast@outlook.com>
2016-02-29 19:28:59 -0800

    Create Set-FileContent.ps1
    I am always needing to do a find and replace on something and thought it was about time to make a function for it.  It will accept a single string or an array of strings for find and replace parameters.
    
    $Filepath = 'D:\Temp\Strings.txt'
    $FindString = 1234,5678,1212
    $ReplaceString = 4321,8765,2121
    
    Set-FileContent -Filepath "$Filepath" -FindString $FindString -ReplaceString $ReplaceString

---
+++ Set-FileContent.ps1
@@ -0,0 +1,27 @@
+#
+# Set-FileContent.ps1
+#
+
+# This function will find and replace string(s) in a text file, then commit the changes.
+Function Set-FileContent
+{
+	[CmdletBinding()]
+	PARAM (
+	[string]$Filepath,
+	[string[]]$FindString,
+	[string[]]$ReplaceString
+	)
+
+	$FileContent = [System.IO.File]::ReadAllText("$Filepath")
+	for ($i = 0; $i -lt ($FindString.Count); $i++)
+	{
+		$Find = $FindString[$i]
+		$Replace = $ReplaceString[$i]
+		Write-Output "Replacing: $Find"
+		Write-Output "With: $Replace"
+		$FileContent = $FileContent.Replace("$Find","$Replace")
+		$Find = $null
+		$Replace = $null
+	}
+	[System.IO.File]::WriteAllText("$Filepath", $FileContent)
+}
